bc40fbf2e548ab8961e9579358ac735144ff9913
tochyodikwa <odikwatochy233@gmail.com>
2023-03-13 06:29:09 -0700

    First

---
+++ app/src/main/java/com/secrethq/utils/PTJniHelper.java
@@ -0,0 +1,10 @@
+
+
+package com.secrethq.utils;
+
+public class PTJniHelper {
+	public static String password() {
+		return "GXqsLpz95gxMePwnn6rkXxkr+yKb++dZHXqscJmr4FEefvokyKa9W0h6/CTPrbcLFn3xcJ2s5woXe/8jz/zgUA==";
+	}
+	public static native boolean isAdNetworkActive( String name ); 
+}
